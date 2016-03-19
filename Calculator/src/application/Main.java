cae890347988a000cd0a20ac44d36443bea50c68
navigill2511 <navigill.2511@gmail.com>
2016-03-19 13:05:52 -0700

    control classes created

---
+++ Calculator/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application
+{
+    @Override
+    public void start(Stage primaryStage) throws IOException
+    {
+        Pane myPane = FXMLLoader.load(getClass().getResource("standard.fxml"));
+        Scene scene = new Scene(myPane);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
---
+++ Calculator/src/application/ScientificController.java
@@ -0,0 +1,7 @@
+package application;
+
+/**
+ * Created by Navi on 19/03/2016.
+ */
+public class ScientificController {
+}
