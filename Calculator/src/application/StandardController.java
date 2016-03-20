5ee8316c7da8862e30eece3b22c14d79b4ed9199
navigill2511 <navigill.2511@gmail.com>
2016-03-20 16:43:10 -0700

    can switch between different views now

--- Calculator/src/application/StandardController.java
+++ Calculator/src/application/StandardController.java
@@ -1,7 +1,53 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class StandardController
 {
+    @FXML private Button button;
+    @FXML private TextField standardTextField;
+
+    private String output;
+    private String temp;
+    private String operator;
+
+    /**
+     *
+     * @param event
+     */
+    @FXML
+    public void number(ActionEvent event)
+    {
+        button = (Button)event.getSource();
+
+        output = (output == null) ? button.getText() : (output + button.getText());
+
+        standardTextField.setText(output);
+    }
+
+    /**
+     *
+     * @param event
+     */
+    @FXML
+    public void operator(ActionEvent event)
+    {
+        button   = (Button)event.getSource();
+        temp     = output;
+        output   = "";
+        operator = button.getText();
+
+        standardTextField.setText(output);
+
+
+    }
+
+    @FXML
+    public void calculate(ActionEvent event)
+    {
+
+    }
 }
