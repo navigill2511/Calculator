4203df7e9de9231d3373109f65a4bf251ecc1c86
navigill2511 <navigill.2511@gmail.com>
2016-03-20 01:02:49 -0700

    fxml files fixed

---
+++ Calculator/src/application/MenuController.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ * Created by Navi on 19/03/2016.
+ */
+public class MenuController
+{
+    @FXML
+    public void handleEvent(ActionEvent event)
+    {
+
+    }
+}
