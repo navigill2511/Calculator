6f8058bd4af00550a3be853256a035f4b6c8921a
navigill2511 <navigill.2511@gmail.com>
2016-03-31 18:19:14 -0700

    started the first model class

---
+++ Calculator/src/application/StandardScientificModel.java
@@ -0,0 +1,34 @@
+package application;
+
+public class StandardScientificModel
+{
+    public long add(long num_1, long num_2)
+    {
+        return num_1 + num_2;
+    }
+
+    public long subtract(long num_1, long num_2)
+    {
+        return num_1 - num_2;
+    }
+
+    public long multipy(long num_1, long num_2)
+    {
+        return num_1 * num_2;
+    }
+
+    public long divide(long num_1, long num_2)
+    {
+        return num_1 / num_2;
+    }
+
+    public long remainder( long num_1, long num_2)
+    {
+        return num_1 % num_2;
+    }
+
+    public long sin(long num)
+    {
+        return
+    }
+}
