7a5c270ab581d87ddc5b5c1e7fba53fb68ef3284
navigill2511 <navigill.2511@gmail.com>
2016-04-05 12:04:20 -0700

    fixed standard functioning

--- Calculator/src/application/StandardController.java
+++ Calculator/src/application/StandardController.java
@@ -11,115 +11,61 @@ import javafx.scene.control.TextField;
 
 public class StandardController
 {
-    private final int MAX_DIGITS = 10;
-
-    @FXML private TextField standardTextField;
-
-    private String output = Object.EMPTY;
-    private String operandOne;
-    private String operandTwo;
-    private String operator;
-
-    private int counter = 0;
+    @FXML private TextField standardTextField;  // TextField as defined in 'Standard.fxml'.
+    private String output;                      // Output to be displayed in TextField.
 
     /**
      *
+     */
+    public StandardController()
+    {
+        this.output = Object.EMPTY;
+    }
+
+    /**
      * @param event
      */
-    @FXML
-    public void operand(ActionEvent event)
+    @FXML public void handleInputOperation(ActionEvent event)
     {
-        // If the screen is full, return.
-        if (this.counter >= MAX_DIGITS) return;
-
-        // If this is the first digit, clear the textField.
-        if (this.counter == 0) this.standardTextField.setText(this.output = Object.CLEAR);
-
         // Get the button that caused the event.
-        Button button = (Button)event.getSource();
+        String button = ((Button)event.getSource()).getText();
 
         // Add the number to the output and display the output.
-        this.output = (this.output == null) ? button.getText() : (this.output + button.getText());
-        this.standardTextField.setText(this.output);
-
-        // Increment number of digits.
-        counter++;
+        this.standardTextField.setText(this.output = (this.output + button));
     }
 
     /**
      *
-     * @param event
      */
-    @FXML
-    public void operator(ActionEvent event)
-    {
-        // If the output is empty, return.
-        if (this.output.equals(Object.EMPTY)) return;
-
-        // Get the operator that caused the event.
-        this.operator = ((Button)event.getSource()).getText();
-
-        // Get operand one.
-        this.operandOne = this.output;
-
-        // Clear output and textField.
-        this.standardTextField.setText(this.output = Object.EMPTY);
-
-        this.counter = 0;   // Empty counter.
-    }
-
-    @FXML
-    public void calculate()
+    @FXML public void calculate()
     {
         // If either operator, operandOne or operandTwo are empty calculation can not be done.
-        if (operator.equals(Object.EMPTY) || operandOne.equals(Object.EMPTY) || output.equals(Object.EMPTY)) return;
+        if (this.output.equals(Object.EMPTY)) return;
 
-        // Get the operand two.
-        this.operandTwo = this.output;
-
-        double operand_1 = Double.parseDouble(this.operandOne);
-        double operand_2 = Double.parseDouble(this.operandTwo);
-        double result    = 0;
-
-        // Perform calculation according to the operator.
-        switch (this.operator)
-        {
-            case (Object.ADD):       result = StandardModel.add      (operand_1, operand_2); break;
-            case (Object.SUBTRACT):  result = StandardModel.subtract (operand_1, operand_2); break;
-            case (Object.MULTIPLY):  result = StandardModel.multipy  (operand_1, operand_2); break;
-            case (Object.DIVIDE):    result = StandardModel.divide   (operand_1, operand_2); break;
-            case (Object.REMAINDER): result = StandardModel.remainder(operand_1, operand_2); break;
-        }
-
-        // Display the result to the textField.
-        standardTextField.setText(this.output = Formatter.round(result, MAX_DIGITS));
-
-        this.operandOne = Object.CLEAR;
-        this.operandTwo = Object.CLEAR;
-        this.counter    = 0;
+        this.standardTextField.setText(this.output = Double.toString(StandardModel.evaluate(this.output)));
     }
 
-    @FXML
-    public void negativePositive()
+    /**
+     *
+     */
+    @FXML public void negativePositive()
     {
-        standardTextField.setText(this.output);
+        standardTextField.setText(this.output = Formatter.negativePositive(this.output));
     }
 
-    @FXML
-    public void resetObjects()
+    /**
+     *
+     */
+    @FXML public void clearScreen()
     {
-        this.operandOne = Object.EMPTY;
-        this.operandTwo = Object.EMPTY;
-
         this.standardTextField.setText(this.output = Object.EMPTY);
-
-        this.counter = 0;
     }
 
-    @FXML
-    public void decimal()
+    /**
+     *
+     */
+    @FXML public void decimal()
     {
         this.standardTextField.setText(this.output = Formatter.addDecimal(this.output));
-        counter++;
     }
 }
--- Calculator/src/application/model/StandardModel.java
+++ Calculator/src/application/model/StandardModel.java
@@ -1,9 +1,19 @@
 package application.model;
+import application.formatter.Object;
+import java.util.ArrayList;
 
 /**
  * Created by Navi on 01/04/2016.
  */
 public class StandardModel {
+
+    public static void main (String[] args)
+    {
+        //if (Object.DIGITS.contains("*")) System.out.println("yay");
+
+        System.out.println(evaluate("232.0*3"));
+    }
+
     public static double add(double x, double y)
     {
         return x + y;
@@ -27,5 +37,196 @@ public class StandardModel {
     public static double remainder(double x, double y)
     {
         return x % y;
+    }
+
+    /**
+     * Evaluate a postfix expression.
+     * @param infix A String that contains an expression in postfix notation.
+     *                The allowable operators are +, -, *, and /.  Operands are
+     *                integers (signed or unsigned).  The operators and operands
+     *                must be surrounded by at least one space.
+     * @return The value of the postfix expression.
+     */
+    public static double evaluate(String infix)
+    {
+        // Obtain the tokens as an array of strings.
+        String postfix = convertToPostfix(infix);
+        String[] tokens = postfix.split(Object.WHITESPACE);
+
+        // Create a stack for the operands.
+        Stack<Double> operands = new Stack<>();
+
+        // Process the tokens.
+        for (String token : tokens)
+        {
+            // If the token is an integer, it is an operand and can be placed on the stack.
+            try
+            {
+                operands.push(Double.parseDouble(token));
+            }
+
+            // If the token is not an operand, it must be an operator.  Remove the top two
+            catch (NumberFormatException nfe)
+            {
+                double operand2 = operands.pop();
+                double operand1 = operands.pop();
+                if (token.equals(Object.ADD))      operands.push(operand1 + operand2);
+                if (token.equals(Object.SUBTRACT)) operands.push(operand1 - operand2);
+                if (token.equals(Object.MULTIPLY)) operands.push(operand1 * operand2);
+                if (token.equals(Object.DIVIDE))   operands.push(operand1 / operand2);
+            }
+            // operands, perform the appropriate operation, and place the result on the stack.
+        }
+        // The expression value is on the stack.
+        return operands.pop();
+    }
+
+    /**
+     * Convert an infix expression to postfix notation.
+     * @param infix A String that contains an expression in infix notation.
+     *              Operators, operands, and brackets must be surrounded by
+     *              at least one space.
+     * @return The postfix expression, as a String, that is equivalent to the infix expression.
+     */
+    public static String convertToPostfix(String infix)
+    {
+        // Obtain the infix expression tokens as an array of strings.
+        ArrayList<String> tokens = new ArrayList<>();
+
+        // Convert the expression into an array of operands and operators first.
+        int digitBeginFlag = 0;
+        for (int i = 0; i < infix.length(); i++)
+        {
+            String currentChar = infix.substring(i, i + 1);
+
+            System.out.println(currentChar);
+            // If the current character is not a digit or a decimal, add it to 'tokens'
+            if (!isDigit(currentChar))
+            {
+                System.out.println("operator: " + currentChar);
+
+                digitBeginFlag = i + 1;
+                tokens.add(currentChar);
+                continue;
+            }
+
+            while (i < infix.length())
+            {
+                currentChar = infix.substring(i, i + 1);
+
+                System.out.println("operand_or_digit: " + currentChar);
+
+                // If current object is not a digit or not a decimal, break;
+                if (!isDigit(currentChar)) break;
+                i++;
+            }
+
+            tokens.add(infix.substring(digitBeginFlag, i));
+            i--;
+        }
+
+        System.out.println(tokens.toString());
+
+        // Convert to postfix and return expression as string.
+        return convertToPostfix(tokens, 0, tokens.size() - 1);
+    }
+
+    /**
+     * Convert an infix expression to postfix notation.
+     * @param infix A String[] for the infix expression where each element of the array contains
+     *              one token of the infix expression.
+     * @param first The index of the first token to be processed.
+     * @param last The index of the last token to be processed.
+     * @return The postfix expression, as a String, that is equivalent to the infix expression.
+     */
+    public static String convertToPostfix(ArrayList<String> infix, int first, int last)
+    {
+        // Create a stack for the operators and a string for the output expression.
+        Stack<String> operators = new Stack<>();
+        String output = "";
+
+        // Process the tokens.
+        while (first <= last)
+        {
+            String token = infix.get(first);
+
+            // If the token is a left parenthesis, find the matching parenthesis and
+            // convert the tokens between the matching parentheses.
+            if (token.equals(Object.LEFT_PARENTHESIS))
+            {
+                int match = findMatchingParenthesis(infix, first + 1, last);
+                output += convertToPostfix(infix, first + 1, match - 1) + " ";
+                first = match;
+            }
+            else
+            {
+                // If the token is an operand, just output the operand.
+                if (!Object.ALL_OPERATORS.contains(token)) output += token + " ";
+                else
+                {
+                    // The token is an operator.  Output those operators on the stack
+                    // that have equal or higher precedence than the token
+                    while (!operators.isEmpty() && (precedence(operators.peek()) >= precedence(token)))
+                    {
+                        output += operators.pop() + " ";
+                    }
+                    operators.push(token);
+                }
+            }
+            // Advance to the next token in the infix expression.
+            first++;
+        }
+
+        // Output any remaining operators.
+        while (!operators.isEmpty()) output += operators.pop() + " ";
+
+        return output;
+    }
+
+    /**
+     * Find the matching parenthesis.
+     * @param tokens A String[] of tokens to be searched.
+     * @param first The index of the first token to be searched.
+     * @param last The index of the last token to be searched.
+     * @return The index number for the matching closing parenthesis token.
+     */
+    private static int findMatchingParenthesis(ArrayList<String> tokens, int first, int last) {
+        int left = 0;
+
+        while (first <= last)
+        {
+            String token = tokens.get(first);
+
+            // Count the number of left parentheses encountered.
+            if (token.equals(Object.LEFT_PARENTHESIS)) left++;
+
+            // When a right parenthesis is encountered, it is the matching one
+            // only after all the intervening left parentheses have been matched.
+            if (token.equals(Object.RIGHT_PARENTHESIS))
+            {
+                if (left == 0) return first;
+                else left--;
+            }
+            first++;
+        }
+        return -1;	// No matching parenthesis.
+    }
+
+    /**
+     * Determine the precedence for an operator.
+     * @param operator An operator.
+     * @return 0 if the operator is + or -; otherwise, return 1.
+     */
+    private static int precedence(String operator)
+    {
+        return Object.ADD_OR_SUBTRACT.indexOf(operator) >= 0 ? 0 : 1;
+    }
+
+    private static boolean isDigit(String character)
+    {
+        return (character.equals("1") || character.equals("2") || character.equals("3") ||
+                character.equals("4") || character.equals("5") || character.equals("6") ||
+                character.equals("7") || character.equals("8") || character.equals("9") ||
+                character.equals("0") || character.equals("."));
     }
 }
