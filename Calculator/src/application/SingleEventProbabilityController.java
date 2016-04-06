dfbfd301be91ffe5ab0e86981fc74f432256a740
navigill2511 <navigill.2511@gmail.com>
2016-04-06 11:08:49 -0700

    single event probability done

--- Calculator/src/application/SingleEventProbabilityController.java
+++ Calculator/src/application/SingleEventProbabilityController.java
@@ -1,7 +1,44 @@
 package application;
+import application.formatter.Object;
+import application.model.StatisticsModel;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 /**
  * Created by Navi on 29/03/2016.
  */
-public class SingleEventProbabilityController {
+public class SingleEventProbabilityController
+{
+    @FXML TextField singleEventProbability;
+    String numberOfOccurrences;
+    String numberOfOutcomes;
+
+    public SingleEventProbabilityController()
+    {
+        this.numberOfOccurrences = Object.EMPTY;
+        this.numberOfOutcomes = Object.EMPTY;
+    }
+
+    @FXML public void handleOccurrenceOperation(Event event)
+    {
+        this.numberOfOccurrences = ((TextField) event.getSource()).getText();
+    }
+
+    @FXML public void handleOutcomeOperation(Event event)
+    {
+        this.numberOfOutcomes = ((TextField) event.getSource()).getText();
+    }
+
+    @FXML public void calculate()
+    {
+        if (this.numberOfOccurrences.equals(Object.EMPTY)
+                || this.numberOfOutcomes.equals(Object.EMPTY)) return;
+
+        double occurrences = Double.parseDouble(this.numberOfOccurrences);
+        double outcomes    = Double.parseDouble(this.numberOfOutcomes);
+        String result   = Double.toString(StatisticsModel.singleEventProbability(occurrences, outcomes));
+
+        this.singleEventProbability.setText(result);
+    }
 }
--- Calculator/src/application/model/StatisticsModel.java
+++ Calculator/src/application/model/StatisticsModel.java
@@ -9,13 +9,16 @@ public class StatisticsModel
 {
     public static void main(String[] args)
     {
-        System.out.println(permutation(new BigInteger("34"), new BigInteger("4")));
+       // System.out.println(permutation(new BigInteger("34"), new BigInteger("4")));
        // System.out.println(factorial(new BigInteger("34")) );/// factorial(34 - 4));
         //System.out.println(factorial(34 - 4));
+
+        System.out.println(singleEventProbability(2, 12));
     }
 
     /**
-     *
+     * Calculates permutations using formula below.
+     * nPr = n! / (n - r)!
      * @param n
      * @param r
      * @return
@@ -26,6 +29,7 @@ public class StatisticsModel
     }
 
     /**
+     * Calculates combination using formula below
      * nCr = n! / r!(n - r)!
      * @param n
      * @param r
@@ -46,5 +50,16 @@ public class StatisticsModel
         BigInteger one = new BigInteger("1");
         if (x.equals(one)) return x;
         return x.multiply(factorial(x.subtract(one)));
+    }
+
+    /**
+     * Single event probability of an event A, P(A) = (#occurences / #outcomes).
+     * @param occurrences
+     * @param outcomes
+     * @return
+     */
+    public static double singleEventProbability(double occurrences, double outcomes)
+    {
+        return (occurrences / outcomes);
     }
 }
